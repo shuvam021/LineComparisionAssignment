530fdba16fddfb393be09ae6cf60206b6a4cc167
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 21:10:52 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+        }
+    }
+}
