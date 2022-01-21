b6eb0dfb2124fe63c008d46f3f63e13ae546b85c
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 21:58:24 +0530

    [Shuvam] Add oops concept implemented

---
+++ LineOperation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionAssignment
+{
+    internal class LineOperation
+    {
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+        public LineOperation(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public double LengthOfLine()
+        {
+            return Math.Sqrt(Math.Pow((this.x2 - this.x1), 2) + Math.Pow((this.y2 - this.y1), 2));
+        }
+        public static void CheckEqual(double val1, double val2)
+        {
+            Console.WriteLine($"{val1} == {val2}: {val1.Equals(val2)}"); ;
+        }
+        public static void Compare(double val1, double val2)
+        {
+            switch (val1.CompareTo(val2))
+            {
+                case 1:
+                    Console.WriteLine($"{val1} > {val2}");
+                    break;
+                case -1:
+                    Console.WriteLine($"{val1} < {val2}");
+                    break;
+                case 0:
+                    Console.WriteLine($"{val1} == {val2}");
+                    break;
+                default:
+                    Console.WriteLine("Error: somehing went wrong");
+                    break;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,39 +4,14 @@ namespace LineComparisionAssignment
 {
     internal class Program
     {
-        public static double LengthOfLine()
-        {
-            Console.Write("Insert x1 = ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Insert y1 = ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Insert x2 = ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Insert y2 = ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert data for 1st line: ");
-            double line1Length = LengthOfLine();
-            Console.WriteLine("Insert data for 2nd line: ");
-            double line2Length = LengthOfLine();
-            switch (line1Length.CompareTo(line2Length))
-            {
-                case 1:
-                    Console.WriteLine($"{line1Length} > {line2Length}");
-                    break;
-                case -1:
-                    Console.WriteLine($"{line1Length} < {line2Length}");
-                    break;
-                case 0:
-                    Console.WriteLine($"{line1Length} == {line2Length}");
-                    break;
-                default:
-                    Console.WriteLine("Error: somehing went wrong");
-                    break;
-            }
+            LineOperation line1 = new LineOperation(5, 6, 8, 9);
+            LineOperation line2 = new LineOperation(6, 7, 12, 17);
+            Console.WriteLine(line1.LengthOfLine());
+            Console.WriteLine(line2.LengthOfLine());
+            LineOperation.CheckEqual(line1.LengthOfLine(), line2.LengthOfLine());
+            LineOperation.Compare(line1.LengthOfLine(), line2.LengthOfLine());
         }
     }
 }
