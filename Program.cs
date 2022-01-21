3b87778bb4f4430d16b5d545cc1c3d8da3d35515
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 21:15:36 +0530

    [Shuvam] Add length calculation of 2 point implemented

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@ namespace LineComparisionAssignment
 {
     internal class Program
     {
+        public static double LengthOfLine()
+        {
+            Console.Write("Insert x1 = ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert y1 = ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert x2 = ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert y2 = ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine($"length of line is {LengthOfLine()} unit");
         }
     }
 }
